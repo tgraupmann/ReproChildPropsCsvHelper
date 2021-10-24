eb2ceb596872cb1c107316991c0a60ecbea4181d
Tim Graupmann <tgraupmann@gmail.com>
2021-10-24 13:15:17 -0700

    Created a small working repro project

---
+++ TestObject.cs
@@ -0,0 +1,11 @@
+﻿namespace ReproChildPropsCSVHelper
+{
+    class TestObject
+    {
+        [CsvHelper.Configuration.Attributes.Name("attribute_first")]
+        public Microsoft.Xrm.Sdk.EntityReference attribute_first { get; set; }
+
+        [CsvHelper.Configuration.Attributes.Name("attribute_second")]
+        public Microsoft.Xrm.Sdk.EntityReference attribute_second { get; set; }
+    }
+}
